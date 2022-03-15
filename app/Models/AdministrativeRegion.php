076d9c197ba6bbc56698e9b9ddfa41d191da08f9
Rute Miliana <rutemiliana13@gmail.com>
2022-03-15 10:57:44 -0300

    view

--- app/Models/AdministrativeRegion.php
+++ app/Models/AdministrativeRegion.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AdministrativeRegion extends Model
 {
     protected $fillable = ['administrative_region_name'];
+    
     use HasFactory;
 }
--- database/migrations/2022_03_14_113012_create_section_processes_table.php
+++ database/migrations/2022_03_14_113012_create_section_processes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
+    { 
         Schema::create('section_processes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
