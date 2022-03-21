10a0503f279d48b1d16cbb604508c66ee76ba850
Rute Miliana <rutemiliana13@gmail.com>
2022-03-21 09:58:04 -0300

    seeder Activity

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Activity;
 
 class ActivitySeeder extends Seeder
 {
@@ -14,6 +15,26 @@ class ActivitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $activities = [            
+            ['id' => 1, 'activity_name' => 'ANÁLISE DE PROJETO'],
+            ['id' => 2, 'activity_name' => 'APOIO A FISCALIZAÇÃO DE OBRAS'],
+            ['id' => 2, 'activity_name' => 'APOIO A FISCALIZAÇÃO DE OBRAS'],
+            ['id' => 2, 'activity_name' => 'APOIO A FISCALIZAÇÃO DE OBRAS'],
+            ['id' => 2, 'activity_name' => 'APOIO A FISCALIZAÇÃO DE OBRAS'],
+            
+        
+            
+        ];
+
+        foreach ($activities as $activity) {
+            Activity::updateOrCreate(['id' => $activity['id']], $activity);
+        }
+
+            /*foreach ($activities as $activity) {
+                Activity::create($activity);
+            }
+            */
+
+       
     }
 }
