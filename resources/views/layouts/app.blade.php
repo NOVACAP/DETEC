c8b8a942bb2129f0b3c1ae412a1e064359f79bf7
Rute Miliana <rutemiliana13@gmail.com>
2022-04-19 11:58:30 -0300

    alter view app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,9 +96,5 @@
         </main>
     </div>
 
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-      <script src="{{asset ('js/bootstrap.min.js')}}"></script>
-      <script src="{{ asset('js/popper.min.js')}}"></script>
-
 </body>
 </html>
